f7974fc0347f75c5ead16581178f73296087c4be
Matías Báez <matiasbaez2512@gmail.com>
2023-11-04 13:00:21 -0400

    Initial pages structure

---
+++ lib/pages/pages.dart
@@ -0,0 +1,4 @@
+
+export 'package:stripe_app/pages/card_page.dart';
+export 'package:stripe_app/pages/home_page.dart';
+export 'package:stripe_app/pages/payment_complete_page.dart';
---
+++ lib/pages/payment_complete_page.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class PaymentCompletePage extends StatelessWidget {
+
+  const PaymentCompletePage({ Key? key }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context){
+    return Scaffold(
+      body: Container(),
+    );
+  }
+}
